95c834fb29b3c4a6b03f69b98b1c007e71422b71
Florent Kermarrec <florent@enjoy-digital.fr>
2015-09-07 12:12:51 +0200

    init repo

---
+++ doc/source/docs/intro/release_notes.rst
@@ -0,0 +1,12 @@
+.. _release-notes:
+
+=============
+Release Notes
+=============
+
+.. _ChangeLog:
+
+ChangeLog
+=========
+0.9.0:
+  - First release supporting Xilinx Kintex7.
---
+++ litesata/frontend/__init__.py
@@ -0,0 +1,5 @@
+from litesata.common import *
+from litesata.frontend.crossbar import LiteSATACrossbar
+from litesata.frontend.arbiter import LiteSATAArbiter
+from litesata.frontend.striping import LiteSATAStriping
+from litesata.frontend.bist import LiteSATABIST
