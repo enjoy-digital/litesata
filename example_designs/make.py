30856edc480fc8abfd6820f261a0498a02f6e5cb
Florent Kermarrec <florent@enjoy-digital.fr>
2016-03-16 19:39:24 +0100

    example_designs: use new Vivado special overrides

--- example_designs/make.py
+++ example_designs/make.py
@@ -159,8 +159,8 @@ System Clk: {} MHz (min: {} MHz)
         soc_fragment = soc.get_fragment()
         platform.finalize(soc_fragment)
         so = {
-            NoRetiming:             XilinxNoRetiming,
-            MultiReg:               XilinxMultiReg,
+            NoRetiming:             XilinxNoRetimingVivado,
+            MultiReg:               XilinxMultiRegVivado,
             AsyncResetSynchronizer: XilinxAsyncResetSynchronizer
         }
         v_output = platform.get_verilog(soc_fragment, name="litesata", special_overrides=so)
