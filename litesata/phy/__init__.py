5a092b9515654bce5d02db5d7718924dfdc05581
Florent Kermarrec <florent@enjoy-digital.fr>
2020-10-28 19:54:31 +0100

    phy: add CSR registers for software control/status.

--- litesata/phy/__init__.py
+++ litesata/phy/__init__.py
@@ -3,7 +3,7 @@ from litesata.phy.ctrl import *
 from litesata.phy.datapath import *
 
 
-class LiteSATAPHY(Module):
+class LiteSATAPHY(Module, AutoCSR):
     """SATA PHY
 
     Manages the low level interface between the SATA core and the device.
@@ -24,6 +24,27 @@ class LiteSATAPHY(Module):
         self.pads   = pads
         self.gen    = gen
 
+        # Registers
+        self.enable = CSRStorage(reset=1)
+        self.status = CSRStatus(fields=[
+            CSRField("ready", size=1, values=[
+                    ("``0b0``", "PHY not initialized."),
+                    ("``0b1``", "PHY initialized and ready.")
+            ]),
+            CSRField("tx_ready", size=1, values=[
+                    ("``0b0``", "TX not initialized."),
+                    ("``0b1``", "TX initialized and ready.")
+            ]),
+            CSRField("rx_ready", size=1, values=[
+                    ("``0b0``", "RX not initialized."),
+                    ("``0b1``", "RX initialized and ready.")
+            ]),
+            CSRField("ctrl_ready", size=1, values=[
+                    ("``0b0``", "Ctrl/OOB not initialized."),
+                    ("``0b1``", "Ctrl/OOB initialized and ready.")
+            ]),
+        ])
+
         # Transceiver / Clocks
         if device[:4] == "xc7k": # Kintex 7
             from litesata.phy.k7sataphy import K7LiteSATAPHYCRG, K7LiteSATAPHY
@@ -35,9 +56,17 @@ class LiteSATAPHY(Module):
             self.submodules.crg = A7LiteSATAPHYCRG(refclk, pads, self.phy, gen)
         else:
             raise NotImplementedError
+        self.comb += [
+            self.phy.tx_init.restart.eq(~self.enable.storage),
+            self.phy.rx_init.restart.eq(~self.enable.storage),
+            self.status.fields.ready.eq(self.phy.ready),
+            self.status.fields.tx_ready.eq(self.phy.tx_init.done),
+            self.status.fields.rx_ready.eq(self.phy.rx_init.done),
+        ]
 
         # Control
         self.submodules.ctrl = LiteSATAPHYCtrl(self.phy, self.crg, clk_freq)
+        self.comb += self.status.fields.ctrl_ready.eq(self.ctrl.ready)
 
         # Datapath
         self.submodules.datapath = LiteSATAPHYDatapath(self.phy, self.ctrl)
