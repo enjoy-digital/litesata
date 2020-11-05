7844bdce77563410e4bf996d4670b06091fc3eaa
Florent Kermarrec <florent@enjoy-digital.fr>
2020-11-05 12:20:22 +0100

    phy: make CSR registers optional (not wanted for standalone core).

--- litesata/phy/__init__.py
+++ litesata/phy/__init__.py
@@ -19,14 +19,55 @@ class LiteSATAPHY(Module, AutoCSR):
     For now, the Kintex7/Zynq(with PL based on K7) PHY is the only one available,
     but the achitecture is modular enough to accept others PHYs.
     """
-    def __init__(self, device, pads, gen, clk_freq, refclk=None, data_width=16):
+    def __init__(self, device, pads, gen, clk_freq, refclk=None, data_width=16, with_csr=True):
         self.pads   = pads
         self.gen    = gen
         self.refclk = refclk
 
-        # Registers
-        self.enable = CSRStorage(reset=1)
-        self.status = CSRStatus(fields=[
+        # Control/Status
+        self.enable = Signal()
+        self.ready  = Signal()
+
+        # Transceiver / Clocks
+        if device[:4] == "xc7k": # Kintex 7
+            from litesata.phy.k7sataphy import K7LiteSATAPHYCRG, K7LiteSATAPHY
+            self.submodules.phy = K7LiteSATAPHY(pads, gen, clk_freq, data_width)
+            self.submodules.crg = K7LiteSATAPHYCRG(refclk, pads, self.phy, gen)
+        elif device[:4] == "xc7a": # Artix 7
+            from litesata.phy.a7sataphy import A7LiteSATAPHYCRG, A7LiteSATAPHY
+            self.submodules.phy = A7LiteSATAPHY(pads, gen, clk_freq, data_width, tx_buffer_enable=True)
+            self.submodules.crg = A7LiteSATAPHYCRG(refclk, pads, self.phy, gen,  tx_buffer_enable=True)
+        else:
+            from litesata.phy.gthsataphy import GTHLiteSATAPHYCRG, GTHLiteSATAPHY
+            self.submodules.phy = GTHLiteSATAPHY(pads, gen, clk_freq, data_width)
+            self.submodules.crg = GTHLiteSATAPHYCRG(refclk, pads, self.phy, gen)
+
+        # Control
+        self.submodules.ctrl = LiteSATAPHYCtrl(self.phy, self.crg, clk_freq)
+
+
+        # Datapath
+        self.submodules.datapath = LiteSATAPHYDatapath(self.phy, self.ctrl)
+        self.comb += [
+            self.ctrl.rx_idle.eq(self.datapath.rx_idle),
+            self.ctrl.misalign.eq(self.datapath.misalign)
+        ]
+        self.sink, self.source = self.datapath.sink, self.datapath.source
+
+        # Restart/Status
+        self.comb += [
+            self.phy.tx_init.restart.eq(~self.enable),
+            self.phy.rx_init.restart.eq(~self.enable | self.ctrl.rx_reset),
+            self.ready.eq(self.phy.ready & self.ctrl.ready),
+        ]
+
+        # CSRs
+        if with_csr:
+            self.add_csr()
+
+    def  add_csr(self):
+        self._enable = CSRStorage(reset=1)
+        self._status = CSRStatus(fields=[
             CSRField("ready", size=1, values=[
                     ("``0b0``", "PHY not initialized."),
                     ("``0b1``", "PHY initialized and ready.")
@@ -45,36 +86,10 @@ class LiteSATAPHY(Module, AutoCSR):
             ]),
         ])
 
-        # Transceiver / Clocks
-        if device[:4] == "xc7k": # Kintex 7
-            from litesata.phy.k7sataphy import K7LiteSATAPHYCRG, K7LiteSATAPHY
-            self.submodules.phy = K7LiteSATAPHY(pads, gen, clk_freq, data_width)
-            self.submodules.crg = K7LiteSATAPHYCRG(refclk, pads, self.phy, gen)
-        elif device[:4] == "xc7a": # Artix 7
-            from litesata.phy.a7sataphy import A7LiteSATAPHYCRG, A7LiteSATAPHY
-            self.submodules.phy = A7LiteSATAPHY(pads, gen, clk_freq, data_width, tx_buffer_enable=True)
-            self.submodules.crg = A7LiteSATAPHYCRG(refclk, pads, self.phy, gen,  tx_buffer_enable=True)
-        else:
-            raise NotImplementedError
-
-        # Control
-        self.submodules.ctrl = LiteSATAPHYCtrl(self.phy, self.crg, clk_freq)
-
-
-        # Datapath
-        self.submodules.datapath = LiteSATAPHYDatapath(self.phy, self.ctrl)
         self.comb += [
-            self.ctrl.rx_idle.eq(self.datapath.rx_idle),
-            self.ctrl.misalign.eq(self.datapath.misalign)
-        ]
-        self.sink, self.source = self.datapath.sink, self.datapath.source
-
-        # Restart/Status
-        self.comb += [
-            self.phy.tx_init.restart.eq(~self.enable.storage),
-            self.phy.rx_init.restart.eq(~self.enable.storage | self.ctrl.rx_reset),
-            self.status.fields.ctrl_ready.eq(self.ctrl.ready),
-            self.status.fields.ready.eq(self.phy.ready & self.ctrl.ready),
-            self.status.fields.tx_ready.eq(self.phy.tx_init.done),
-            self.status.fields.rx_ready.eq(self.phy.rx_init.done),
+            self.enable.eq(self._enable.storage),
+            self._status.fields.ready.eq(self.phy.ready & self.ctrl.ready),
+            self._status.fields.tx_ready.eq(self.phy.tx_init.done),
+            self._status.fields.rx_ready.eq(self.phy.rx_init.done),
+            self._status.fields.ctrl_ready.eq(self.ctrl.ready),
         ]
