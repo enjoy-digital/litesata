97ebfb02d9ef06185ffa8b71b6e05b1355ea88d3
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 09:29:38 +0100

    setup.py: Improve indentation.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from setuptools import setup
@@ -5,18 +6,18 @@ from setuptools import find_packages
 
 
 setup(
-    name="litesata",
-    description="Small footprint and configurable SATA core",
-    author="Florent Kermarrec",
-    author_email="florent@enjoy-digital.fr",
-    url="http://enjoy-digital.fr",
-    download_url="https://github.com/enjoy-digital/litesata",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
-    entry_points={
+    name                 = "litesata",
+    description          = "Small footprint and configurable SATA core",
+    author               = "Florent Kermarrec",
+    author_email         = "florent@enjoy-digital.fr",
+    url                  = "http://enjoy-digital.fr",
+    download_url         = "https://github.com/enjoy-digital/litesata",
+    test_suite           = "test",
+    license              = "BSD",
+    python_requires      = "~=3.6",
+    packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data = True,
+    entry_points         = {
         "console_scripts": [
             "litesata_gen=litesata.gen:main",
         ],
